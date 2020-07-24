dcc371a7355e643d78f0e2eb380939ec35e46b2b
Chetan Dhangar <chetansdhangar@gmail.com>
2020-07-24 12:06:22 +0530

    Home Component with React router

---
+++ src/components/Logo/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.css';
+
+/**
+* @author
+* @function Logo
+**/
+
+const Logo = (props) => {
+  return(
+    <div className="logo">
+        <a href="#">Chetan Dhangar</a>
+    </div>
+   )
+
+ }
+
+export default Logo
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react';
+import './style.css';
+import Card from '../UI/Card';
+
+import blogPost from '../../data/blog.json';
+import { NavLink } from 'react-router-dom';
+
+/**
+* @author
+* @function Sidebar
+**/
+
+const Sidebar = (props) => {
+
+    const [posts, setPosts] = useState([]);
+    
+    
+    useEffect(() => {
+        const posts = blogPost.data;
+        setPosts(posts);
+    }, [posts]);
+
+
+
+  return(
+      <div className="sidebarContainer" style={{
+          width: props.width
+      }}>
+            <Card style={{ marginBottom: '20px', padding: '20px', boxSizing: 'border-box' }}>
+                <div className="cardHeader">
+                    <span>About Us</span>
+                </div>
+                <div className="profileImageContainer">
+                    <img src="" alt="Progile Image" />
+                </div>
+                <div className="cardBody">
+                    <p className="personalBio">My name is Chetan Dhangar I am a software developer specialization in Front end developement....:)</p>
+                </div>
+            </Card>
+
+            <Card style={{ marginBottom: '20px', padding: '20px', boxSizing: 'border-box' }}>
+                <div className="cardHeader">
+                    <span>Social Network</span>
+                </div>
+            </Card>
+
+            <Card style={{ marginBottom: '20px', padding: '20px', boxSizing: 'border-box' }}>
+                <div className="cardHeader">
+                    <span>Recent Posts</span>
+                </div>
+
+                <div className="recentPosts">
+
+                    {
+                        posts.map(post => {
+                            return (
+                                <NavLink key={post.id} to={`/post/${post.slug}`}>
+                                    <div className="recentPost">
+                                        <h3>{post.blogTitle}</h3>
+                                        <span>{post.postedOn}</span>
+                                    </div>
+                                </NavLink>
+                                
+                            );
+                        })
+                    }
+                </div>
+
+            </Card>
+      </div>
+    
+   )
+
+ }
+
+export default Sidebar
