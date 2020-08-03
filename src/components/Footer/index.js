98cd841a5e92b1bc12a32184dcce73cc5bb3fb66
Chetan Dhangar <chetansdhangar@gmail.com>
2020-08-03 15:12:48 +0530

    image added

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,16 +4,40 @@ import { NavLink } from 'react-router-dom';
  
  const Footer = (props) =>{
      return(
-         <footer className="footer">
-             <div className="footer-nav">
-             <ul className="footer-navmenu">
-            <li><NavLink to="/">Home</NavLink></li>
-            <li><NavLink to="/about-us">About Us</NavLink></li>
-            <li><NavLink  to="/post">Posts</NavLink></li>
-            <li><NavLink  to="/contact-us">Contact Us</NavLink></li>
-        </ul>
-             </div>
-         </footer>
+         <footer>
+            <div className="row mt-5">
+                <div className="col-auto">
+                    <diV></diV>
+                </div>
+            </div>
+            <div className="container mt-5">
+                <div className="row row-content">
+                    <div className="col-12 col-md-6 mr-auto">
+                       <h3>Let's Build Something Toghether</h3>
+                       <p></p>
+                       <p></p>
+                       <p>Feel free to reach if you are looking for a developer,have a question or just want to Connect.</p>
+                    </div>
+                    <div className="col-12 col-md-5">
+                        <h3></h3>
+                        <p className="text-center">Email:<a className="text-secondary" href="chetansdhanagar@gmail.com"> chetansdhangar@gmail.com</a></p>
+                        <div className="text-center">
+                        <a className="btn btn-socail-icon btn-instagram" href="http://instagram.com/chetan.o21"><i className="fa fa-instagram"></i></a>
+                        <a className="btn btn-socail-icon btn-github" href="http://github.com/Chetandhangar"><i className="fa fa-github"></i></a>
+                        <a className="btn btn-socail-icon btn-linkedin" href="https://www.linkedin.com/in/chetandhangar/"><i className="fa fa-linkedin"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="container mt-5">
+            <div className="row justify-content-center">  
+                    <div className="col-auto text-fade row-content">
+                        <p>© Chetan Dhangar 2020. All rights Reserve.</p>
+                        <span className="text-center"><p>Made with ❤️ in India.</p></span>
+                    </div>
+                </div>
+            </div>
+        </footer>
      );
 
 
--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -31,7 +31,7 @@ const Sidebar = (props) => {
                     <span>About Us</span>
                 </div>
                 <div className="profileImageContainer">
-                    <img src="" alt="Progile Image" />
+                    <img src="https://instagram.fnag1-2.fna.fbcdn.net/v/t51.2885-19/s150x150/102293229_602841493668536_6352726465050208883_n.jpg?_nc_ht=instagram.fnag1-2.fna.fbcdn.net&_nc_ohc=Ix1OJMc22UIAX9se2xY&oh=ca51688e10491e6e9fc825411b863c0d&oe=5F506264" alt="Progile Image" />
                 </div>
                 <div className="cardBody">
                     <p className="personalBio">My name is Chetan Dhangar I am a software developer specialization in Front end developement....:)</p>
@@ -41,11 +41,6 @@ const Sidebar = (props) => {
             <Card style={{ marginBottom: '20px', padding: '20px', boxSizing: 'border-box' }}>
                 <div className="cardHeader">
                     <span>Social Network</span>
-                </div>
-                <div className="cardBody">
-                    <button>Email</button>
-                    <button>Instagram</button>
-                    <button>FaceBook</button>
                 </div>
             </Card>
 
